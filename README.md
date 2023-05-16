cbd95ba94867cab96c4a78303ef67e8e672e29ed
Nick <burkettng@appstate.edu>
2023-05-16 00:17:32 -0400

    Frist contact with GitHub

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # removeDuplicatesFromSortedArray
 This is a Leet Code problem I am working on.
+
+The problem is to remove duplicate values in a sorted array 
+and replace them with a underscore at the end of the array
+
+Given example A: 
+	input array  = [1, 1, 2, 3, 3, 4]
+	output array = [1, 2, 3, 4, _, _]
+
+Given example B: 
+	input array  = [1, 2, 2, 4, 5, 5, 6]
+	output array = [1, 2, 4, 5, 6, _, _]
+
+Note: that the underscores always go at the end of the array and need to equal 
+     the number of duplicate values found in the array.
---
+++ removeDuplicates.py
@@ -0,0 +1,27 @@
+"""
+Author: Nicholas G. Burkett
+Date: 05/15/2023
+Problem: Remove duplicate values from a sorted array
+Expected input:  [1, 1, 2, 3, 3, 4]
+Expected output: [1, 2, 3, 4, _, _]
+Level: Easy
+"""
+
+#Here I will create a class for this problem
+class Solution(object):	
+    
+
+	#Need to create a function that will remove the duplicates
+	def removeDuplicates(self, nums): 
+		"""
+		:type nums: List[int]
+		:type: int
+		"""
+
+    	#Here I will define a main method to run the show
+	def main():
+		#Here I will create test data and see what works
+		print("First contact")
+	#Now I need to call the main method
+	if __name__ == "__main__": 
+		main()		 
