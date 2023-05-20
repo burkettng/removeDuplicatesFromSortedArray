3ae36b79eada7c35d6c1eb72cb7da8a19b1632ea
Nick <burkettng@appstate.edu>
2023-05-20 12:25:05 -0400

    This is failing of triplets and greater

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -10,9 +10,8 @@ Level: Easy
 #Here I will create a class for this problem
 class Solution(object):	
     
-
 	#Need to create a function that will remove the duplicates
-	def removeDuplicates(nums): 
+	def removeDuplicates(self, nums): 
 		"""
 		:type nums: List[int]
 		:type: int
@@ -25,16 +24,15 @@ class Solution(object):
 		#Create a while loop to step through the array
 		while(boolVar):
 			#First I will perfrom a bounds check on a pointer
-			if(nums[i] == (lenOfArr - 1)):
-				boolVar = false	  #Set the boolVar to false becasue I am done
+			if(i == (lenOfArr - 1)):
+				boolVar = False	  #Set the boolVar to false becasue I am done
 
 			#I will check to see if we have a duplicate pair
-		
 			elif(nums[i] == nums[i+1]):
 				#If I fall in here I know I need to alter the array... aka replace some values
 				#Here I will create a for loop to step through and alter the array
 				itr = i+1     #Declare a second counter variable for the for-loop
-				for i in range(itr, itr < lenOfArr - 1, 1): 
+				for itr in range(itr, (lenOfArr - 1)): 
 					#Possibly need to check bounds here, but I don't think I do... we will see
 					nums[itr] = nums[itr+1]    #Perform the swap
 
@@ -45,26 +43,24 @@ class Solution(object):
 			#...i.e -> nums[i] != nums[i+=1]
 			else: 
 				i += 1    #Increment the main counter and keep moving
-				 	
-		return nums	
+
+		return nums 	
     	#Here I will define a main method to run the show
-	def main():
+	def main(self):
 		#Here I will create test data and see what works
 		#First I need to create a couple test arrays		
-		testArr01 = [1, 1, 2, 3, 3, 4, 5]    #Test array 1
+		testArr01 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]    #Test array 1
 		testArr02 = [1, 2, 2, 3, 4]          #Test array 2
 		testArr03 = [1, 2, 3, 4, 5, 6, 7]    #Test array 3
-		
-		#Next I need to create and instance of my class
-		test = Solution()
 		
 		print("Entering testing...")	     #Display message to user stating they have started the process	
 		
 		#Here is where I test array 1
-		print(Solution.removeDuplicates(testArr01))   #Display the result of test 1
-		print(Solution.removeDuplicates(testArr02))   #Display the result of test 2
-		print(Solution.removeDuplicates(testArr03))   #Display the result of test 3
-
+		print(test.removeDuplicates(testArr01))   #Display the result of test 1
+		#print(test.removeDuplicates(testArr02))   #Display the result of test 2
+		#print(test.removeDuplicates(testArr03))   #Display the result of test 3
+ 
 #Now I need to call the main method
 if __name__ == "__main__": 
-	Solution.main()		 
+	test = Solution()
+	test.main()		 
